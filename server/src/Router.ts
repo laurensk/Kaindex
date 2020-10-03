1dc6f006d813bc64436d51634f9fc609ceb54016
Laurens K <39766854+laurensk@users.noreply.github.com>
2020-10-04 01:11:00 +0200

    refactored router

--- server/src/Router.ts
+++ server/src/Router.ts
@@ -1,14 +1,14 @@
-import carsRouter from "./routes/User";
-import classroomsRouter from "./routes/Classrooms";
-import departmentsRouter from "./routes/Departments";
-import teachersRouter from "./routes/Teachers";
-import userRouter from "./routes/User";
+import cars from "./routes/cars";
+import classrooms from "./routes/classrooms";
+import departments from "./routes/departments";
+import teachers from "./routes/teachers";
+import user from "./routes/user";
 
 import express from "express";
 export const router = express.Router();
 
-router.use("/cars", carsRouter);
-router.use("/classrooms", classroomsRouter);
-router.use("/departments", departmentsRouter);
-router.use("/teachers", teachersRouter);
-router.use("/user", userRouter);
+router.use("/cars", cars);
+router.use("/classrooms", classrooms);
+router.use("/departments", departments);
+router.use("/teachers", teachers);
+router.use("/user", user);
