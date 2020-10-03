45ec29277a5037f808bc6bf3122330fcae83ed44
Laurens K <39766854+laurensk@users.noreply.github.com>
2020-10-03 21:31:56 +0200

    teacher model

---
+++ server/src/models/Teacher.model.ts
@@ -0,0 +1,12 @@
+export class Teacher {
+  uuid: string;
+  title: string;
+  firstName: string;
+  lastName: string;
+  short: string;
+  mail: string;
+  imageUrl: string;
+  active: boolean;
+  modifiedDate: Date;
+  creationDate: Date;
+}
