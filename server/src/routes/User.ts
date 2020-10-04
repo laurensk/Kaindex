da298e988b2fc4322da92ac542598eaaa34da6e1
Laurens K <39766854+laurensk@users.noreply.github.com>
2020-10-04 19:29:59 +0200

    updated user router

--- server/src/routes/User.ts
+++ server/src/routes/User.ts
@@ -1,8 +1,36 @@
 import express from "express";
+import { Error } from "../api/helpers/ErrorHandling";
+import { verifyToken } from "../api/user/verifyToken";
+import Joi from "@hapi/joi";
+import { getUser } from "../api/user/getUser";
+import { loginUser } from "../api/user/loginUser";
 const router = express.Router();
 
-router.route("/").get((res, req) => {
-  req.send("hello");
+router.route("/").get(verifyToken, (req, res) => {
+  const uuid = req.body.uuid;
+  getUser(uuid, (error, user) => {
+    if (error) return Error.sendError(res, error);
+    res.json({
+      user: user,
+    });
+  });
+});
+
+router.route("/login").post((req, res) => {
+  const validation = Joi.object({
+    login: Joi.string().required(),
+    password: Joi.string().required(),
+  });
+  const { error } = validation.validate(req.body);
+  if (error) return Error.sendError(res, Error.validationError);
+
+  loginUser(req.body.email, req.body.password, (error, user, token) => {
+    if (error) return Error.sendError(res, error);
+    res.header("auth-token", token);
+    res.json({
+      user: user,
+    });
+  });
 });
 
 export default router;
