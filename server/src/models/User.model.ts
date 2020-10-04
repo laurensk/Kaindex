fd8b976b69d7c109ec9210a64c4cdbfe925a2c3e
Laurens K <39766854+laurensk@users.noreply.github.com>
2020-10-04 19:29:51 +0200

    updated user model

--- server/src/models/User.model.ts
+++ server/src/models/User.model.ts
@@ -1,17 +1,15 @@
 export class User {
   uuid: string;
+  name: string;
   login: string;
   admin: boolean;
   active: boolean;
-  creationDate: Date;
-  lastLoginDate: Date;
 
-  constructor(uuid: string, login: string, admin: boolean, active: boolean, creationDate: Date, lastLoginDate: Date) {
+  constructor(uuid: string, name: string, login: string, admin: boolean, active: boolean) {
     this.uuid = uuid;
+    this.name = name;
     this.login = login;
     this.admin = admin;
     this.active = active;
-    this.creationDate = creationDate;
-    this.lastLoginDate = lastLoginDate;
   }
 }
